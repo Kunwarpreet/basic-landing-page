f7d45a477ebee9c7254d20a3363abed8000c0368
Kunwarpreet <singhkunwarpreet797@gmail.com>
2024-08-03 18:18:37 +0530

    push project

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.getElementById('cta-button');
+
+    ctaButton.addEventListener('click', function() {
+        alert('Thank you for clicking! We will get in touch with you soon.');
+    });
+});
